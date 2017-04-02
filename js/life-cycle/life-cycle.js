9973c6404c68b283a4aa354f08df6e6556a34b09
Sureddi, Manoj (ESI) <manoj.wolverine8@gmail.com>
2017-04-02 15:31:39 +0530

    Changed the diagonals logic

--- js/life-cycle/life-cycle.js
+++ js/life-cycle/life-cycle.js
@@ -11,78 +11,65 @@ class Lifecycle{
       else if(player2==3) return {winner:"Player2"}
       else return null;
   }
-  static checkDiagonals(){
-
-  }
-
-  static checkColumns(){
-
-  }
-
-  static checkRows(){
-
-  }
-
-  static checkGameOver(){
-    var player1=0,player2=0;
-    let gameState=[],result=null;
-
-    $("#tictactoe tr").each(function() {
-    		gameState.push($(this).find('td'));
-    });
+  static checkDiagonals(gameState){
+    let player1=0,player2=0;
     for (var i = 0; i < gameState.length; i++) {
       if($(gameState[i][i]).hasClass("player1")){
         player1++
       }else if($(gameState[i][i]).hasClass("player2")){
         player2++
       }
-      console.log("\n\ndiaglnal 1");
-
-      console.log(player1);
-      console.log(player2);
-        result=this.checkScore(player1,player2);
+        let result=this.checkScore(player1,player2);
         if(result) return result;
-  }
-
-  for (var i = 0; i < gameState.length; i++) {
-    player1=0;player2=0;
-    if( $(gameState[i][gameState[i].length-i]).hasClass("player1")){
-      player1++
-    }else if($(gameState[i][gameState[i].length-i]).hasClass("player2")){
-      player2++
     }
-    console.log("\n\ndiaglnal 2");
 
-    console.log(player1);
-    console.log(player2);
-    result=this.checkScore(player1,player2);
-    if(result) return result;
-  }
-
-  for (var i = 0; i < gameState.length; i++) {
     player1=0;player2=0;
 
-    for (var j = 0; j < gameState[i].length; j++) {
-      if($(gameState[j][i]).hasClass("player1")) player1++;
-      if($(gameState[j][i]).hasClass("player2")) player2++;
+    for (var i = 0; i < gameState.length; i++) {
+      if( $(gameState[i][gameState[i].length-i-1]).hasClass("player1")){
+        player1++
+      }else if($(gameState[i][gameState[i].length-i-1]).hasClass("player2")){
+        player2++
+      }
+      let result=this.checkScore(player1,player2);
+      if(result) return result;
     }
-    console.log("\n\ncolumns ",i);
-
-    console.log(player1);
-    console.log(player2);
-    result=this.checkScore(player1,player2);
-    if(result) return result;
   }
 
-  for (var i = 0; i < gameState.length; i++) {
-    player1=0;player2=0;
+  static checkColumns(gameState){
+    let player1=0,player2=0;
+    for (var i = 0; i < gameState.length; i++) {
+      player1=0;player2=0;
 
-    for (var j = 0; j < gameState[i].length; j++) {
-      if($(gameState[i][j]).hasClass("player1")) player1++;
-      if($(gameState[i][j]).hasClass("player2")) player2++;
+      for (var j = 0; j < gameState[i].length; j++) {
+        if($(gameState[j][i]).hasClass("player1")) player1++;
+        if($(gameState[j][i]).hasClass("player2")) player2++;
+      }
+      let result=this.checkScore(player1,player2);
+      if(result) return result;
     }
-    result=this.checkScore(player1,player2);
-    if(result) return result;
   }
+
+  static checkRows(gameState){
+    let player1=0,player2=0;
+    for (var i = 0; i < gameState.length; i++) {
+      player1=0;player2=0;
+
+      for (var j = 0; j < gameState[i].length; j++) {
+        if($(gameState[i][j]).hasClass("player1")) player1++;
+        if($(gameState[i][j]).hasClass("player2")) player2++;
+      }
+      let result=this.checkScore(player1,player2);
+      if(result) return result;
+    }
+  }
+
+  static checkGameOver(){
+    let gameState=[],result=null;
+
+    $("#tictactoe tr").each(function() {
+    		gameState.push($(this).find('td'));
+    });
+    return this.checkDiagonals(gameState) || this.checkRows(gameState) || this.checkColumns(gameState);
 }
 }
